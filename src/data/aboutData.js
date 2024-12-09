5ce8d0e71e70317661ec093cacd3d38774bb8288
Wenjia Liu <78522560+serenawenjia@users.noreply.github.com>
2024-12-09 01:09:21 -0500

    about text

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,7 +1,7 @@
 export const aboutData = {
   title: "Greetings, I'm Wenjia Liu",
   description1:
-    "I'm currently a 4th year student at McGill University. My major is Computer Science and my minor is Economics and I'm graduating in May 2023. I'm applying to HCI/UX graduate programs in the US.",
+    "I'm currently a 4th year student at McGill University. My major is Computer Science and my minor is Economics. I'm graduating in May 2023 and I'm applying to HCI/UX graduate programs in the US.",
   description2: "",
   image: 1,
 };
