6477897728effaac4f9dc4e4165c7ab5ceda0481
Wenjia Liu <78522560+serenawenjia@users.noreply.github.com>
2024-12-09 01:09:20 -0500

    remove abu from code

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,7 +1,7 @@
 export const aboutData = {
   title: "Who I am", // TODO
   description1:
-    "My name is ABU SAID. I'm a web designer and developer based in Dhaka, Bangladesh.",
+    "My name is Wenjia Liu. I'm a web designer and developer based in Dhaka, Bangladesh.",
   description2:
     "I am a professional and passionate programmer in my daily life. A quick learner with a self-learning attitude. I love to learn and explore new technologies and am Passionate about Problem Solving. Love almost all the stacks of Software Engineering. My current stack includes Nodejs, Nestjs, React, NextJs, Redux, Bootstrap, Ant Design, Material UI, MongoDB, Mysql, PostgreSQL, Firebase, Cloudinary, etc.",
   image: 1,
--- src/data/socialsData.js
+++ src/data/socialsData.js
@@ -1,9 +1,9 @@
 export const socialsData = {
-  github: "https://github.com/said7388", // TODO
-  facebook: "https://www.facebook.com/abusaid.riyaz/",
-  linkedIn: "https://www.linkedin.com/in/abu-said-bd/",
-  twitter: "https://twitter.com/said7388",
-  medium: "https://medium.com/@abusaid-bd",
-  stackOverflow: "https://stackoverflow.com/users/16840768/abu-said",
-  mail: "abusaid7388@gmail.com",
+  github: "", // TODO
+  facebook: "",
+  linkedIn: "",
+  twitter: "",
+  medium: "",
+  stackOverflow: "",
+  mail: "",
 };
