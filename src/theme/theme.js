f3dfee5dd04458dd5e799dcbc3d1a923e62d9d90
Wenjia Liu <78522560+serenawenjia@users.noreply.github.com>
2024-12-09 01:09:19 -0500

    light theme color notes

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -1,24 +1,22 @@
 /* eslint-disable */
-import { contactsGreen } from './images'
+import { contactsGreen } from "./images";
 
 export const theLightTheme = {
-    type: 'light',
-    primary: '#1D9BF0',
-    quaternary: '#EFF3F4',
-    quaternaryLight: '#F7F9F9',
-    secondary: '#ffffff',
-    tertiary: '#0f1419',
-    buttonColor: '#0F1419',
-    contactsimg: contactsGreen
-}
+  type: "light",
+  primary: "#1D9BF0", // some text
+  quaternary: "#EFF3F4", // left panel
+  secondary: "#ffffff", // background
+  tertiary: "#0f1419", // some text
+  buttonColor: "#0F1419", // View all button
+  contactsimg: contactsGreen,
+};
 
 export const theDarkTheme = {
-    type: 'dark',
-    primary: '#1D9BF0',
-    quaternary: '#1E2732',
-    quaternaryLight: '#8b98a5',
-    secondary: '#15202B',
-    tertiary: '#EFF3F4',
-    buttonColor: '#8B98A5',
-    contactsimg: contactsGreen
-}
+  type: "dark",
+  primary: "#1D9BF0",
+  quaternary: "#1E2732",
+  secondary: "#15202B",
+  tertiary: "#EFF3F4",
+  buttonColor: "#8B98A5",
+  contactsimg: contactsGreen,
+};
