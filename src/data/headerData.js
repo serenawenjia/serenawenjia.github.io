75ee6b098a9c65d3f28c9e5b6419ed9d66b9792e
Wenjia Liu <78522560+serenawenjia@users.noreply.github.com>
2024-12-09 01:09:21 -0500

    intro

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -3,9 +3,9 @@ import image from "../assets/images/wenjia.jpg";
 export const headerData = {
   name: "Greetings, I'm Wenjia Liu",
   desciption:
-    "I'm a 4th year Computer Science student at McGill University graduating in May 2023. I'm applying to HCI/UX graduate programs in the US.",
-  desciption2: "",
-  desciption3: "",
+    "I'm a 4th year Computer Science student at McGill University graduating in May 2023.",
+  desciption2: "Passionate about UI/UX, HCD, and Problem Solving.  ",
+  desciption3: "Think and create ( ๑╹ ꇴ╹)✧",
   image: image,
   cvUrl:
     "https://drive.google.com/file/d/1NsN8EVO-0B9po-BHpbCZeczlwRzplvzK/view",
