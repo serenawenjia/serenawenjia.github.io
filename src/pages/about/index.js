0e78c5b92985412af223174da99097c2c3dcf5b6
Wenjia Liu <78522560+serenawenjia@users.noreply.github.com>
2024-12-09 01:09:20 -0500

    more

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -1,16 +1,9 @@
 import React from "react";
 import { Helmet } from "react-helmet";
-import Blog from "../../components/container/blog";
-import Contacts from "../../components/container/contacts";
-import Landing from "../../components/container/landing";
 import Navbar from "../../components/container/navbar";
-import Projects from "../../components/container/projects";
 import About from "../../components/core-ui/about/about";
-import Certificate from "../../components/core-ui/certificate/certificate";
 import Education from "../../components/core-ui/education/education";
-import Experience from "../../components/core-ui/experience/experience";
 import Skills from "../../components/core-ui/skills/skills";
-import { headerData } from "../../data/headerData";
 
 function AboutPage() {
   return (
