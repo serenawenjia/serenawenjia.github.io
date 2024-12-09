538998f321eb0de3622be2ef95a17b6923716892
Wenjia Liu <78522560+serenawenjia@users.noreply.github.com>
2024-12-09 01:09:20 -0500

    navbar sticky

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -2,86 +2,12 @@ import surveyeasy from "../assets/images/surveyeasy/cover.png";
 
 export const projectsData = [
   {
-    // TODO
     id: 1,
     projectName: "Research, Design, & Development: SurveyEasy Website",
     projectDesc:
       "This project is about designing and implementing a website that is easy-to-use for both researchers and survey participants.",
-    tags: ["Figma", "React"], // 写role 还是用到的工具？
+    tags: ["UX Research", "Product Design", "Web Development"], // 写role 还是用到的工具？
     demo: "/projects/surveyeasy",
     image: surveyeasy,
   },
-  {
-    id: 2,
-    projectName: "SOCS website for McGill University",
-    projectDesc:
-      "This project is a Jewelry shop, Customer can be buy product and manage them and make payment.",
-    tags: ["React", "Express", "Material Ui"],
-    demo: "/projects/ta-management",
-    image: "",
-  },
-  {
-    id: 3,
-    projectName: "Travel Agency",
-    projectDesc: "This project about travel agency management.",
-    tags: ["React", "CSS", "Material Ui"],
-    demo: "https://travel-agency-fc58b.web.app/",
-    image: "",
-  },
-  {
-    id: 4,
-    projectName: "Android Patient Tracker",
-    projectDesc:
-      "This project involves the development of an Android application for viewing and managing patient data.",
-    tags: ["Flutter", "Firebase"],
-    demo: "https://github.com/hhhrrrttt222111/developer-portfolio",
-    image: "",
-  },
-  {
-    id: 5,
-    projectName: "E-Commerce App",
-    projectDesc: "A Simple E-commerce application",
-    tags: ["React Native", "Firebase"],
-    demo: "https://github.com/hhhrrrttt222111/developer-portfolio",
-    image: "",
-  },
-  {
-    id: 6,
-    projectName: "Uber Lite",
-    projectDesc: "Uber clone",
-    tags: ["Flutter"],
-    demo: "https://github.com/hhhrrrttt222111/developer-portfolio",
-    image: "",
-  },
-  {
-    id: 7,
-    projectName: "Stock Market App",
-    projectDesc: "A simple stock market API app",
-    tags: ["React", "Redux", "Bootstrap"],
-    demo: "https://github.com/hhhrrrttt222111/developer-portfolio",
-    image: "",
-  },
-  {
-    id: 8,
-    projectName: "Car Pooling System",
-    projectDesc:
-      "The carpooling system merges multiple people in a car which leads to meet new people, reduces pollution",
-    tags: ["Flutter", "React"],
-    demo: "https://github.com/hhhrrrttt222111/developer-portfolio",
-    image: "",
-  },
 ];
-
-// Do not remove any fields.
-// Leave it blank instead as shown below
-
-/* 
-{
-    id: 1,
-    projectName: 'Car Pooling System',
-    projectDesc: '',
-    tags: ['Flutter', 'React'],
-    demo: '',
-    image:''
-}, 
-*/
