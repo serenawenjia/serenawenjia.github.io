967664298096dea8db6e24ea76e50229a2796332
Wenjia Liu <78522560+serenawenjia@users.noreply.github.com>
2024-12-09 01:09:20 -0500

    ..

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,10 +1,12 @@
+import image from "../assets/images/wenjia.png";
+
 export const headerData = {
   name: "Wenjia",
   title: "Hello! I am",
   desciption:
     "Hello! I am Wenjia, a professional and passionate programmer in my daily life. A quick learner with a self-learning attitude. I love to learn and explore new technologies and am Passionate about Problem Solving. ",
-  image: "https://i.ibb.co/ZmdNH6x/abu-said.jpg",
-  imagebw: "https://i.ibb.co/Cw2Xp90/abu-said-bw.jpg",
+  image: image, //TODO
+  imagebw: image,
   resumePdf:
     "https://docs.google.com/document/d/1OmxnrxHbrfQkL7e3BG40HgSmYCWzNSxo9Wa-kOPVPSc/edit?usp=sharing",
 };
